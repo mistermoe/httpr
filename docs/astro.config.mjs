e1cfb5bfa81dd7deb4ef331d76a70fc740d46c1f
Moe Jangda <moe@tbd.email>
2024-10-04 15:17:06 -0500

    change docs font from `roboto-mono` to `lato`

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -10,7 +10,18 @@ export default defineConfig({
 		starlight({
 			title: 'httpr',
 			customCss: [
-				'@fontsource-variable/roboto-mono/index.css',
+				'@fontsource/lato/index.css',
+				'@fontsource/lato/100.css',
+				'@fontsource/lato/100-italic.css',
+				'@fontsource/lato/300.css',
+				'@fontsource/lato/300-italic.css',
+				'@fontsource/lato/700.css',
+				'@fontsource/lato/700-italic.css',
+				'@fontsource/lato/900.css',
+				'@fontsource/lato/900-italic.css',
+				'@fontsource/lato/latin.css',
+				'@fontsource/lato/latin-ext.css',
+				'@fontsource/lato/latin-italic.css',
 				'./src/styles/custom.css'
 			],
 			social: {
